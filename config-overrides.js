65911e6549f30aeca7cf6797999848cf06e56b6b
CRASE <ncn14501@gmail.com>
2025-05-24 16:22:50 +0700

    for jules

--- config-overrides.js
+++ config-overrides.js
@@ -1,31 +1,18 @@
-/* config-overrides.js */
-
 const { override, fixBabelImports, addLessLoader } = require("customize-cra");
-
-/*
-module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.worker\.js$/,
-    use: { loader: "worker-loader" }
-  });
-  return config;
-};
-*/
 
 module.exports = override(
   fixBabelImports("import", {
     libraryName: "antd",
     libraryDirectory: "es",
-    style: 'css' // Changed from true to 'css'
+    style: true // Changed from 'css' to true
   }),
-  addLessLoader({ // This addLessLoader might now be less critical if antd CSS is used
-    lessOptions: { // Options for less-loader v6+
-      javascriptEnabled: true,
-      modifyVars: {
-        "primary-color": "#dc4478",
-        "link-color": "#e91e63",
-        "processing-color": "#e91e63"
-      }
+  addLessLoader({
+    // Options for less-loader v5
+    javascriptEnabled: true,
+    modifyVars: {
+      "primary-color": "#dc4478",
+      "link-color": "#e91e63",
+      "processing-color": "#e91e63"
     }
   })
 );
